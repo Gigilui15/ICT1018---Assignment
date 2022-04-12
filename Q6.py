2b93882a3f09689bba9370eeb0a14881e7604816
= <luigi.naudi@gmail.com>
2022-04-12 18:40:19 +0200

    SieveOfEratosthenes update

--- Q6.py
+++ Q6.py
@@ -18,6 +18,29 @@ def Prime_Checker(num):
     else:
         print(str(num) +" is NOT Prime")     
 
+def SieveOfEratosthenes(n):
+	prime = [True for i in range(n+1)]
+	p = 2
+	while (p * p <= n):
+		# If prime[p] is not changed, then it is a prime
+		if (prime[p] == True):
+
+			# Update all multiples of p
+			for i in range(p * p, n+1, p):
+				prime[i] = False
+		p += 1
+
+	# Print all prime numbers
+	for p in range(2, n+1):
+		if prime[p]:
+			print(p)
+   
+# Driver code
+	n = 20
+	print("Following are the prime numbers smaller than or equal to", n)
+	SieveOfEratosthenes(n)
+
+
 num = rd.randint(1,1000)
 Prime_Checker(803) 
 #803 marked as bad                        
