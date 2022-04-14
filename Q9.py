a3a259482532c4212b6e65d51ec2f29e3a60a74e
= <luigi.naudi@gmail.com>
2022-04-14 20:08:51 +0200

    Q9 Almost There

--- Q9.py
+++ Q9.py
@@ -1,1 +1,16 @@
-#g
+array = [1,2,3,4,5,6,7,8,9,10,11,1,2,3,4,6,7,8,9,11]
+not_repeated = set()
+
+for x in array:
+    print(x)
+    current_length = len(not_repeated)
+    not_repeated.add(x)
+    if((current_length + 1) == len(not_repeated)):
+        array.remove(x)
+     
+if(len(not_repeated) == len(array)):
+    print("There are no repeated elements in this Array")
+else:
+    print("The repeated values in the Array are:\n" ,array)  
+    
+#repeated should be [1,2,3,4,6,7,8,9,11]    
