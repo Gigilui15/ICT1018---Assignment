3d29b5d2200cbdb9ae06583f9d5a821933a67e01
= <luigi.naudi@gmail.com>
2022-04-15 01:39:23 +0200

    Q10 Done but copied completely

--- Q10.py
+++ Q10.py
@@ -1,16 +1,17 @@
-nums = [1,2,3,13,5,6,30,8,14]
+nums = [1,2,55,3,13,5,6,30,8,14]
 
 def find_largest(list):
     #largest element is the only element
     if len(list) == 1:
         return list[0]
     else:
-        print(list[1:])
-        m = find_largest(list[1:])
-        print(m)
-        if m > list[0]:
-            return m  
+        current_largest = find_largest(list[1:])
+        if current_largest > list[0]:
+            return current_largest  
         else:
             return list[0]
 
 print("Largest Number in List: " ,find_largest(nums))
+
+#Reference:
+#https://stackoverflow.com/questions/12711397/python-recursive-function-to-find-the-largest-number-in-the-list
