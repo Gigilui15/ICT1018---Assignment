2d47034969478cfe0442949c79c6512d0beacd29
= <luigi.naudi@gmail.com>
2022-04-15 00:05:28 +0200

    Q9 Done

--- Q9.py
+++ Q9.py
@@ -1,16 +1,23 @@
-array = [1,2,3,4,5,6,7,8,9,10,11,1,2,3,4,6,7,8,9,11]
+array = [1,2,3,13,5,6,51,7,30,8,12,9,10,11,1,2,3,4,6,13,7,8,9,11,50,51]
+#repeated should be 1, 2, 3, 6, 7, 8, 9, 11, 13, 51 
+'''
+#array = [1,2,3,4,5,6,7,8,9,10,11,24,67,41,72]
+#should have no repeated elements
+'''
+repeated = set()
 not_repeated = set()
 
 for x in array:
-    print(x)
     current_length = len(not_repeated)
     not_repeated.add(x)
-    if((current_length + 1) == len(not_repeated)):
-        array.remove(x)
+    #After Adding Each element to set, I am checking if length grew to detect if set accepted element or if it is repeated
+    if(len(not_repeated) != current_length + 1):
+        repeated.add(x)
      
+#Checking if length of non repeated elements is the same as original array      
 if(len(not_repeated) == len(array)):
     print("There are no repeated elements in this Array")
 else:
-    print("The repeated values in the Array are:\n" ,array)  
+    print("The repeated values in the Array are:\n" ,list(repeated))  
     
-#repeated should be [1,2,3,4,6,7,8,9,11]    
+#repeated should be 1, 2, 3, 6, 7, 8, 9, 11, 13, 51   
