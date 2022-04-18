15eaa53452fa451510f25f612c590604f91329e4
= <luigi.naudi@gmail.com>
2022-04-18 17:56:49 +0200

    comment change

--- Q9.py
+++ Q9.py
@@ -19,5 +19,4 @@ if(len(not_repeated) == len(array)):
     print("There are no repeated elements in this Array")
 else:
     print("The repeated values in the Array are:\n" ,list(repeated))  
-    
-#repeated should be 1, 2, 3, 6, 7, 8, 9, 11, 13, 51   
+      
