0a49afd3c8a2dc94f1684d4a13a18d6b69ef4728
= <luigi.naudi@gmail.com>
2022-04-18 17:23:16 +0200

    Documentation and Q8

--- Q8.py
+++ Q8.py
@@ -1,1 +1,16 @@
-#g
+def newtonRaphson(n, iter):
+
+    a = float(n) # number to get square root of
+    for i in range(iter):
+        n =(n + a / n)/2 
+    return n
+
+#number of iterataions
+iter = 10
+
+"""
+Testing Task 8
+"""
+
+print (newtonRaphson(9,iter))
+print (newtonRaphson(2,iter))
