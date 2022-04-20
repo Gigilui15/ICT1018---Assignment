fc5554cd780e18592f2d03d4ac2533a17e7d1485
= <luigi.naudi@gmail.com>
2022-04-20 17:53:37 +0200

    Documentation, Q10 updates +References

--- Q10.py
+++ Q10.py
@@ -8,14 +8,13 @@ def find_largest(list):
     #largest element is the only element
     if len(list) == 1:
         return list[0]
+    
     else:
         current_largest = find_largest(list[1:])
         if current_largest > list[0]:
-            return current_largest  
+            return current_largest 
+         
         else:
             return list[0]
 
 print("Largest Number in List: " ,find_largest(nums))
-
-#Reference:
-#https://stackoverflow.com/questions/12711397/python-recursive-function-to-find-the-largest-number-in-the-list
